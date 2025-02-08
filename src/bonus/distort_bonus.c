4e8bcd17e91185945b42e732ac09b903af80ecb6
vide-sou <victorhugods.pereira@gmail.com>
2025-02-08 11:23:11 -0300

    isolando a isometria

--- src/bonus/distort_bonus.c
+++ src/bonus/distort_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vide-sou <vide-sou@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 08:36:00 by vide-sou          #+#    #+#             */
-/*   Updated: 2025/02/08 10:27:03 by vide-sou         ###   ########.fr       */
+/*   Updated: 2025/02/08 11:22:51 by vide-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,51 @@ double	ft_per_pixel(double value, t_section *section)
 	return (value * section->per_pixel * section->zoom);
 }
 
+void	ft_isometric(t_section *section, double *x, double *y, double z)
+{
+	double	tmp;
+	double	inner_x;
+	double	inner_y;
+
+	inner_x = *x;
+	inner_y = *y;
+	tmp = ((inner_x) * cos(section->angle)) - ((inner_y)
+			* sin(section->angle));
+	*x = ft_per_pixel(tmp * cos(section->persp), section);
+	tmp = ((inner_x) * sin(section->angle)) + ((inner_y)
+			* cos(section->angle));
+	*y = ft_per_pixel(tmp * sin(section->persp) - z, section);
+}
+
 void	ft_centralize(double *x, double *y, double z, t_section *section)
 {
 	double	div_x;
 	double	div_y;
-	double	tmp;
 
-	tmp = ((section->column - 1) * cos(section->angle)) - ((section->row - 1)
-			* sin(section->angle));
-	div_x = (tmp * cos(section->persp));
-	tmp = ((section->column - 1) * sin(section->angle)) + ((section->row - 1)
-			* cos(section->angle));
-	div_y = (tmp * sin(section->persp) - z);
-	*x = (section->width / 2) - (ft_per_pixel(div_x / 2, section));
-	*y = (section->height / 2) - (ft_per_pixel(div_y / 2, section));
+	div_x = section->column - 1;
+	div_y = section->row - 1;
+	ft_isometric(section, &div_x, &div_y, z);
+	*x = (section->width / 2) - (div_x / 2);
+	*y = (section->height / 2) - (div_y / 2);
 	*x += section->diff_x;
 	*y += section->diff_y;
 }
 
 t_pixel_source	ft_distort_pixel(int x, int y, t_section *section)
 {
-	int		index;
-	double	inner_z;
 	double	inner_x;
 	double	inner_y;
-	double	tmp;
+	double	outer_x;
+	double	outer_y;
+	int		index;
 
 	index = x + (y * (section->column + 1));
-	inner_z = section->map[index].z;
-	ft_centralize(&inner_x, &inner_y, inner_z, section);
-	tmp = (x * cos(section->angle)) - (y * sin(section->angle));
-	inner_x += ft_per_pixel(tmp * cos(section->persp), section);
-	tmp = (x * sin(section->angle)) + (y * cos(section->angle));
-	inner_y += ft_per_pixel(tmp * sin(section->persp) - inner_z, section);
+	ft_centralize(&inner_x, &inner_y, section->map[index].z, section);
+	outer_x = x;
+	outer_y = y;
+	ft_isometric(section, &outer_x, &outer_y, section->map[index].z);
+	inner_x += outer_x;
+	inner_y += outer_y;
 	return (ft_create_pixel_source(inner_x, inner_y,
 			section->map[index].color));
 }
